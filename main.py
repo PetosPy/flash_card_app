16f079cdca703af09a8344959a88698740c72f98
Michael Haraeb <haraebm@gmail.com>
2021-04-26 21:27:36 -0300

    Card flip to english updated

--- main.py
+++ main.py
@@ -9,26 +9,29 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 my_file = pd.read_csv("./data/french_words.csv")
 pd_dictionary = my_file.to_dict(orient="records")
-
+current_card = {}
 
 def next_card():
-	card = random.choice(pd_dictionary)
-	canvas.itemconfigure(card_title, text="French")
-	canvas.itemconfigure(card_word, text=card["French"])
+	global current_card
+	current_card = random.choice(pd_dictionary)
+	canvas.itemconfigure(card_title, text="French", fill="black")
+	canvas.itemconfigure(card_word, text=current_card["French"], fill="black")
+	canvas.itemconfig(back_image, image=front_img )
+	
 
 
-	# canvas.itemconfig(front_img, image=back_image )
-	# canvas.itemconfigure(card_title, text="English")
-	# canvas.itemconfigure(card_word, text=card["English"])
-
-
-
+def flip_card():
+	canvas.itemconfigure(card_title, text="English", fill="white")
+	canvas.itemconfigure(card_word, text=current_card["English"], fill="white")
+	canvas.itemconfig(front_img, image=back_image )
+	
 
 #--------------- UI setup ------------#
 window =  Tk()
 window.title("Flash")
 window.config(padx=50, pady=50)
 window.config(bg=BACKGROUND_COLOR)
+window.after(3000, func=flip_card )
 
 canvas = Canvas(window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 
@@ -37,6 +40,7 @@ front_img = canvas.create_image(400, 263,image=flash_img)
 canvas.grid(row=0, column=0, columnspan=2)
 
 back_image = PhotoImage(file="./images/card_back.png")
+
 #-------- text on canvas ------
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 265, text="", font=("Ariel", 60, "bold"))
